c70d484c7c1617224203eaf6feeb78b45d5aa7e7
qz757 <qq757qz@gmail.com>
2016-12-07 22:36:42 +0800

    modify copyblog.sh

--- copyblog.sh
+++ copyblog.sh
@@ -1,1 +1,2 @@
-scp $1 qz757@192.168.2.21:/home/qz757/qz757.github.com/source/_posts/
+scp $1 qz757@192.168.2.21:/home/qz757/qz757.github.com/blogdata/
+#SCP -P 9097 $1 qz757@url:/home/qz757/qz757.github.com/blogdata/
