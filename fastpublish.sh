05c9e6366701716658db54f6bcaab9b7d3a04ebb
qz757 <qq757qz@gmail.com>
2016-12-08 21:34:24 +0800

    ci

--- fastpublish.sh
+++ fastpublish.sh
@@ -2,18 +2,6 @@ cd newblog
 path=`pwd`
 token=$1
 gitpath=$2
-mdlist=`find ./ -name "*.md"`
-mds=""
-for md in $mdlist
-do
-mds=$mds,$md
-done
-echo $mds
-echo "$mds"|grep -q "md"
-if [ $? -eq 0 ]
-then
-echo "publish to blogdata"
-cd ..
 cd ..
 git clone "https://${token}@${gitpath}"
 cd qz757.github.com
@@ -22,4 +10,3 @@ git add -A
 git commit -m "auto publish"$mds
 git push "https://${token}@${gitpath}" hexo:hexo
 echo "publish to web"
-fi
